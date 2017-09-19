778f60e003de85a5d2ef32c4e4b569afbac8b11f
weiphpdev <476974493@qq.com>
2017-09-19 11:08:22 +0800

    完善HTTPS的判断

--- ThinkPHP/Library/Vendor/jssdk/jssdk.php
+++ ThinkPHP/Library/Vendor/jssdk/jssdk.php
@@ -10,9 +10,9 @@ class JSSDK {
 		$jsapiTicket = $this->getJsApiTicket ();
 		
 		// 注意 URL 一定要动态获取，不能 hardcode.
-		$protocol = (! empty ( $_SERVER ['HTTPS'] ) && $_SERVER ['HTTPS'] !== 'off' || $_SERVER ['SERVER_PORT'] == 443) ? "https://" : "http://";
+		$protocol = (! empty ( $_SERVER ['HTTPS'] ) && $_SERVER ['HTTPS'] !== 'off' || $_SERVER ['SERVER_PORT'] == 443||(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) ? "https://" : "http://";
 		$url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-		
+	
 		$timestamp = time ();
 		$nonceStr = $this->createNonceStr ();
 		

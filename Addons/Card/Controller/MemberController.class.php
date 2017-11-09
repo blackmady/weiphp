a0964f05a714adc5966721115708d2f28f8d055c
weiphpdev <476974493@qq.com>
2017-11-09 16:13:18 +0800

    解决选择余额支付，余额不足也会在手机端添加支付记录

--- Addons/Card/Controller/MemberController.class.php
+++ Addons/Card/Controller/MemberController.class.php
@@ -19,21 +19,21 @@ class MemberController extends BaseController {
 		$btn [0] ['is_buttion'] = 1;
 		$btn [0] ['class'] = 'ajax-post confirm';
 		$btn [0] ['url'] = addons_url ( 'Card://Member/changStatus', array (
-				'set_status' => 1 ,
-		          'mdm'=>$_GET['mdm']
+				'set_status' => 1,
+				'mdm' => $_GET ['mdm'] 
 		) );
 		$btn [1] ['title'] = '批量冻结';
 		$btn [1] ['is_buttion'] = 1;
 		$btn [1] ['class'] = 'ajax-post confirm';
 		$btn [1] ['url'] = addons_url ( 'Card://Member/changStatus', array (
 				'set_status' => 2,
-		          'mdm'=>$_GET['mdm']
+				'mdm' => $_GET ['mdm'] 
 		) );
 		
 		$btn [2] ['title'] = '导出会员';
 		$btn [2] ['is_buttion'] = 1;
 		$btn [2] ['class'] = 'export_member';
-		$btn [2] ['url'] = addons_url ( 'Card://Member/export_member' ,$this->get_param);
+		$btn [2] ['url'] = addons_url ( 'Card://Member/export_member', $this->get_param );
 		$this->assign ( 'top_more_button', $btn );
 		
 		$phone = $_REQUEST ['phone'];
@@ -52,8 +52,11 @@ class MemberController extends BaseController {
 			);
 			unset ( $_REQUEST ['number'] );
 		}
-		if (!isset($map['number'])){
-		    $map['number']=array('neq','');
+		if (! isset ( $map ['number'] )) {
+			$map ['number'] = array (
+					'neq',
+					'' 
+			);
 		}
 		$map ['token'] = get_token ();
 		session ( 'common_condition', $map );
@@ -74,7 +77,7 @@ class MemberController extends BaseController {
 	function changStatus() {
 		$ids = I ( 'ids' );
 		if (empty ( $ids )) {
-			$this->error( '400048:请选择要冻结的会员' );
+			$this->error ( '400048:请选择要冻结的会员' );
 		}
 		$setStatus = I ( 'get.set_status' );
 		$map ['id'] = array (
@@ -123,13 +126,13 @@ class MemberController extends BaseController {
 				$recharge = I ( 'recharge' );
 				$map1 ['id'] = $member_id = $_POST ['member_id'];
 				M ( 'card_member' )->where ( $map1 )->setInc ( 'recharge', $recharge );
-				if (is_install("ShopReward")) {
-                    // 充值赠送活动
-                    $this->_send_reward(I('event_id'), $member_id, 'card_recharge_condition', 'recharge_reward');
-                }
-				$this->success ( '保存' . $model ['title'] . '成功！', U ( 'lists?model=' . $this->model ['name'],$this->get_param ) );
+				if (is_install ( "ShopReward" )) {
+					// 充值赠送活动
+					$this->_send_reward ( I ( 'event_id' ), $member_id, 'card_recharge_condition', 'recharge_reward' );
+				}
+				$this->success ( '保存' . $model ['title'] . '成功！', U ( 'lists?model=' . $this->model ['name'], $this->get_param ) );
 			} else {
-				$this->error( '400050:'. $Model->getError () );
+				$this->error ( '400050:' . $Model->getError () );
 			}
 		} else {
 			$map ['manager_id'] = $this->mid;
@@ -151,13 +154,13 @@ class MemberController extends BaseController {
 		if ($_POST ['card_id']) {
 			$_POST ['member_id'] = $_POST ['card_id'];
 		} else if (empty ( $_POST ['card_id'] ) && empty ( $_POST ['number'] )) {
-			$this->error( '400051:请输入会员卡号' );
+			$this->error ( '400051:请输入会员卡号' );
 		} else if ($_POST ['number']) {
 			$map ['number'] = $_POST ['number'];
 			$map ['token'] = get_token ();
 			$id = M ( 'card_member' )->where ( $map )->getField ( 'id' );
 			if (empty ( $id )) {
-				$this->error( '400052:会员卡不存在' );
+				$this->error ( '400052:会员卡不存在' );
 			} else {
 				$_POST ['member_id'] = $id;
 			}
@@ -186,16 +189,14 @@ class MemberController extends BaseController {
 			);
 			if ($type == 0) { // 获取充值赠送活动信息
 				$event_info = M ( 'card_recharge' )->where ( $map )->order ( 'id desc' )->find ();
-				$data ['event_id'] = $event_info ['id'];
+				$data ['event_id'] = intval ( $event_info ['id'] );
 				$data ['event_title'] = $event_info ['title'];
 			} elseif ($type == 1) { // 获取消费赠送活动信息
-			    if (is_install("ShopReward")) {
-                    $event_info = M('shop_reward')->where($map)
-                        ->order('id desc')
-                        ->find();
-                    $data['event_id'] = $event_info['id'];
-                    $data['event_title'] = $event_info['title'];
-                }
+				if (is_install ( "ShopReward" )) {
+					$event_info = M ( 'shop_reward' )->where ( $map )->order ( 'id desc' )->find ();
+					$data ['event_id'] = intval ( $event_info ['id'] );
+					$data ['event_title'] = $event_info ['title'];
+				}
 			}
 		}
 		echo json_encode ( $data );
@@ -223,7 +224,7 @@ class MemberController extends BaseController {
 			}
 			
 			if ($config ['managerPassword'] != $_POST ['pay_password']) {
-				$this->error( '400053:消费密码不正确' );
+				$this->error ( '400053:消费密码不正确' );
 			}
 			if ($_POST ['sn_id']) {
 				$code = M ( 'sn_code' )->find ( $_POST ['sn_id'] );
@@ -240,13 +241,13 @@ class MemberController extends BaseController {
 				if ($_POST ['sn_id']) {
 					D ( 'Common/SnCode' )->set_use ( $_POST ['sn_id'] );
 				}
-                if (is_install("ShopReward")) {
-                    // 消费赠送活动
-                    $this->_send_reward(I('event_id'), $member_id, 'shop_reward_condition', 'shop_reward');
-                }
-				$this->success ( '保存' . $model ['title'] . '成功！', U ( 'lists?model=' . $this->model ['name'],$this->get_param ) );
+				if (is_install ( "ShopReward" )) {
+					// 消费赠送活动
+					$this->_send_reward ( I ( 'event_id' ), $member_id, 'shop_reward_condition', 'shop_reward' );
+				}
+				$this->success ( '保存' . $model ['title'] . '成功！', U ( 'lists?model=' . $this->model ['name'], $this->get_param ) );
 			} else {
-				$this->error( '400054:'. $Model->getError () );
+				$this->error ( '400054:' . $Model->getError () );
 			}
 		} else {
 			$map ['manager_id'] = $this->mid;
@@ -258,35 +259,34 @@ class MemberController extends BaseController {
 				$this->assign ( 'all_number', $allNumber );
 			}
 			$cardMember = M ( 'card_member' )->find ( $data ['member_id'] );
-			if(is_install('ShopCoupon')){
-			    $branch = M ( 'coupon_shop' )->where ( $map )->getFields ( 'id,name' );
-			    $map2 ['uid'] = $cardMember ['uid'];
-			    $map2 ['addon'] = 'ShopCoupon';
-			    $map2 ['can_use'] = 1;
-			    $snCode = M ( 'sn_code' )->where ( $map2 )->getFields ( 'id,sn,target_id,prize_title' );
-			    if ($snCode) {
-			        foreach ( $snCode as $s ) {
-			            $conponArr [$s ['target_id']] = $s ['target_id'];
-			        }
-			        $map3 ['id'] = array (
-			            'in',
-			            $conponArr
-			        );
-			        $conpons = M ( 'shop_coupon' )->where ( $map3 )->getFields ( 'id,title,member' );
-			        foreach ( $snCode as $v ) {
-			            if ($conpons[$v['target_id']]){
-			                $memberArr=explode(',', $conpons[$v['target_id']]['member']);
-			                if (in_array(0, $memberArr) || in_array(-1, $memberArr) || in_array($cardMember['level'], $memberArr)){
-			                    $codeArr ['coupon_title'] = $conpons [$v ['target_id']];
-			                    $couponData[$v['target_id']]=$conpons[$v['target_id']]['title'];
-			                }
-			            }
-			            	
-			        }
-			    
-			        $this->assign ( 'coupon', $couponData );
-			    }
-			    $this->assign ( 'shop', $branch );
+			if (is_install ( 'ShopCoupon' )) {
+				$branch = M ( 'coupon_shop' )->where ( $map )->getFields ( 'id,name' );
+				$map2 ['uid'] = $cardMember ['uid'];
+				$map2 ['addon'] = 'ShopCoupon';
+				$map2 ['can_use'] = 1;
+				$snCode = M ( 'sn_code' )->where ( $map2 )->getFields ( 'id,sn,target_id,prize_title' );
+				if ($snCode) {
+					foreach ( $snCode as $s ) {
+						$conponArr [$s ['target_id']] = $s ['target_id'];
+					}
+					$map3 ['id'] = array (
+							'in',
+							$conponArr 
+					);
+					$conpons = M ( 'shop_coupon' )->where ( $map3 )->getFields ( 'id,title,member' );
+					foreach ( $snCode as $v ) {
+						if ($conpons [$v ['target_id']]) {
+							$memberArr = explode ( ',', $conpons [$v ['target_id']] ['member'] );
+							if (in_array ( 0, $memberArr ) || in_array ( - 1, $memberArr ) || in_array ( $cardMember ['level'], $memberArr )) {
+								$codeArr ['coupon_title'] = $conpons [$v ['target_id']];
+								$couponData [$v ['target_id']] = $conpons [$v ['target_id']] ['title'];
+							}
+						}
+					}
+					
+					$this->assign ( 'coupon', $couponData );
+				}
+				$this->assign ( 'shop', $branch );
 			}
 			
 			$this->assign ( 'data', $data );
@@ -344,12 +344,12 @@ class MemberController extends BaseController {
 				$data ['uid'] = $uid;
 				$data ['score'] = $_POST ['score'];
 				$data ['experience'] = 0;
-				$data ['title'] ='手动修改' ;
+				$data ['title'] = '手动修改';
 				add_credit ( 'card_member_update_score', 5, $data, $id );
 				
-				$this->success ( '保存' . $model ['title'] . '成功！', U ( 'lists?model=' . $this->model ['name'] ,$this->get_param) );
+				$this->success ( '保存' . $model ['title'] . '成功！', U ( 'lists?model=' . $this->model ['name'], $this->get_param ) );
 			} else {
-				$this->error( '400056:'. $Model->getError () );
+				$this->error ( '400056:' . $Model->getError () );
 			}
 		} else {
 			$map ['manager_id'] = $this->mid;
@@ -370,17 +370,17 @@ class MemberController extends BaseController {
 	}
 	// 通用插件的编辑模型
 	public function edit() {
-		$model= $this->model;
+		$model = $this->model;
 		is_array ( $model ) || $model = $this->getModel ( $model );
 		$id || $id = I ( 'id' );
 		
 		// 获取数据
 		$data = M ( get_table_name ( $model ['id'] ) )->find ( $id );
-		$data || $this->error( '400057:数据不存在！' );
+		$data || $this->error ( '400057:数据不存在！' );
 		
 		$token = get_token ();
 		if (isset ( $data ['token'] ) && $token != $data ['token'] && defined ( 'ADDON_PUBLIC_PATH' )) {
-			$this->error( '400058:非法访问！' );
+			$this->error ( '400058:非法访问！' );
 		}
 		
 		if (IS_POST) {
@@ -388,21 +388,21 @@ class MemberController extends BaseController {
 			// 获取模型的字段信息
 			$Model = $this->checkAttr ( $Model, $model ['id'] );
 			if ($Model->create () && $Model->save ()) {
-				//$this->_saveKeyword ( $model, $id );
-				D('Addons://Card/Card')->updateERPMember($_POST,$data['uid']);
+				// $this->_saveKeyword ( $model, $id );
+				D ( 'Addons://Card/Card' )->updateERPMember ( $_POST, $data ['uid'] );
 				// 清空缓存
 				method_exists ( $Model, 'clear' ) && $Model->clear ( $id, 'edit' );
 				
 				$this->success ( '保存' . $model ['title'] . '成功！', U ( 'lists?model=' . $model ['name'], $this->get_param ) );
 			} else {
-				$this->error( '400059:'. $Model->getError () );
+				$this->error ( '400059:' . $Model->getError () );
 			}
 		} else {
 			$fields = get_model_attribute ( $model ['id'] );
 			$this->assign ( 'fields', $fields );
 			$this->assign ( 'data', $data );
 			
-			$this->display (  );
+			$this->display ();
 		}
 	}
 	
@@ -437,17 +437,17 @@ class MemberController extends BaseController {
 				$fieldStr .= $ff . ',';
 			}
 		}
-		//dump($fieldName);exit;
+		// dump($fieldName);exit;
 		$fieldStr = substr ( $fieldStr, 0, strlen ( $fieldStr ) - 1 );
-		$fieldStr.=",uid";
-		//dump($fieldStr);exit;
+		$fieldStr .= ",uid";
+		// dump($fieldStr);exit;
 		// die;
 		// $fieldStr='number,username,phone,recharge,cTime,status';
 		$list_data = M ( 'card_member' )->where ( $map )->field ( $fieldStr )->select ();
-		//dump($list_data);exit;
-		//$uInfo = getUserInfo ( $this->mid );
-		//dump($uInfo);exit;
-		//$levelInfo = D ( 'CardLevel' )->getCardMemberLevel ( $this->mid );
+		// dump($list_data);exit;
+		// $uInfo = getUserInfo ( $this->mid );
+		// dump($uInfo);exit;
+		// $levelInfo = D ( 'CardLevel' )->getCardMemberLevel ( $this->mid );
 		
 		foreach ( $list_data as &$vo ) {
 			$uInfo = getUserInfo ( $vo ['uid'] );
@@ -484,14 +484,11 @@ class MemberController extends BaseController {
 				$fieldArr ['status'] = '状态';
 			}
 			
-				$fieldArr ['score'] = '剩余积分';
-			
+			$fieldArr ['score'] = '剩余积分';
 			
 			if (isset ( $vo ['level'] )) {
 				$fieldArr ['level'] = '会员等级';
 			}
-			
-			
 		}
 		
 		// foreach ($fieldName as $f){
@@ -529,11 +526,11 @@ class MemberController extends BaseController {
 			$titleArr [] = $vv;
 		}
 		$dataArr [] = $titleArr;
-		//dump($list_data);exit;
+		// dump($list_data);exit;
 		// dump($fieldArr);exit;
 		// die;
 		foreach ( $list_data as $v ) {
-			unset($v['uid']);
+			unset ( $v ['uid'] );
 			$dataArr [] = $v;
 		}
 		// vendor ( 'out-csv' );
@@ -544,9 +541,9 @@ class MemberController extends BaseController {
 	
 	// 活动赠送
 	function _send_reward($event_id, $member_id, $table, $credit_type) {
-	    if (!is_install("ShopReward")) {
-	        return false;
-	    }
+		if (! is_install ( "ShopReward" )) {
+			return false;
+		}
 		if (empty ( $event_id )) {
 			return false;
 		}
@@ -568,7 +565,7 @@ class MemberController extends BaseController {
 			$credit ['score'] = intval ( $reward ['score_param'] );
 			add_credit ( $credit_type, 0, $credit );
 		}
-		if ($reward ['shop_coupon'] && is_install("ShopCoupon")) { // 送优惠券
+		if ($reward ['shop_coupon'] && is_install ( "ShopCoupon" )) { // 送优惠券
 			D ( 'Addons://ShopCoupon/ShopCoupon' )->sendCoupon ( $reward ['shop_coupon_param'], $this->mid );
 		}
 	}
